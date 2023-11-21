cd5db87684885f77b721294ed0c29223624687e3
Varun Singh <104829215+VarunSingh19@users.noreply.github.com>
2023-11-21 20:59:42 +0530

    deploying

---
+++ src/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="flex-shrink-0 py-2 bg-dark text-white-50 fixed-bottom">
+    <div className="container text-center">
+      <small>Copyright &copy; TakeNotes by <a href='https://github.com/VarunSingh19' target='_blank'><strong>Varun Singh</strong></a> </small>
+    </div>
+  </footer>
+  )
+}
+
+export default Footer;
